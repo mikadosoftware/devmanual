2ee495189e9f7b1ecbe0d1bd795976aa189f6e49
Paul R Brian <paul@mikadosoftware.com>
2022-09-13 07:48:00 +0100

    update index

---
+++ docs/conceptchapters/explore_patch_calendar.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def test():
+    x=y=0.1
+    fig1 = plt.figure()
+    ax = fig1.add_subplot(100, aspect="equal")
+    patch 
+
+
+if __name__ == '__main__':
+    test()
