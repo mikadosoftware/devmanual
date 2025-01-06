81e38c21e327e58ba9bcb54c389bd5e68128d90e
Paul Brian <paul@mikadosoftware.com>
2025-01-06 20:37:45 +0000

    new format

---
+++ docs/quick_extract_chapters.py
@@ -0,0 +1,32 @@
+'''
+SO I have a big file that I am kind of trying to split the work out into 144 parts
+
+12 parts are *
+144 are ** after a *
+
+so parse it a bit
+'''
+
+def foo():
+    f = 'index.pre'
+    with open(f) as fo:
+        txt = fo.read()
+    currenttop=None
+    resultsd = {}
+    for line in txt.split("\n"):
+        line=line.strip()
+        if line.startswith('*') and not line.startswith('**'):
+            currenttop=line
+            resultsd.setdefault(line,[])
+        if line.startswith('**'):
+           resultsd[currenttop].append(line)
+    show(resultsd)
+
+def show(resultsd):
+    for key, vals in resultsd.items():
+        print(key)
+        for idx, val in enumerate(vals):
+            print(f"  {idx}"+ val)
+ 
+if __name__ == '__main__':
+    foo()
